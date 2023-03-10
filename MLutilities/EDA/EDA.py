7e338c445d92a5b7bd2269c4dc9be7566ccb05be
Daniel Ocampo-Henao <daniel.ocampoh@udea.edu.co>
2023-03-09 21:28:48 -0500

    fix seaborn import

--- MLutilities/EDA/EDA.py
+++ MLutilities/EDA/EDA.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 import statsmodels.stats.api as sms
 import statsmodels.formula.api as smf
